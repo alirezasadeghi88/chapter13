698ff04222200e51420ef32d5b7a0e10935f8bb2
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-05 06:17:23 -0800

    add SortComparableObjects

---
+++ src/homework2/SortComparableObjects.java
@@ -0,0 +1,20 @@
+package homework2;
+
+import java.math.BigInteger;
+
+public class SortComparableObjects {
+    public static void main(String[] args) {
+        String[] cities = {"Savannah", "Boston", "Atlanta", "Tampa"};
+        java.util.Arrays.sort(cities);
+        for (String city: cities)
+            System.out.println(city + " ");
+        System.out.println();
+
+        BigInteger[] hugeNumbers = {new BigInteger("2323231092923992"),
+            new BigInteger("432232323239292"),
+            new BigInteger("54623239292")};
+        java.util.Arrays.sort(hugeNumbers);
+        for (BigInteger number: hugeNumbers)
+            System.out.println(number + " ");
+    }
+}
